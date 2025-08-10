b3dce62d07d6e0ac621cd998924e139859c3fa73
vijay0607 <162467942+codeXAI06@users.noreply.github.com>
2025-08-10 10:49:00 +0530

    Update vite.config.js base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio-2/'
+  base: '/portfolio/'
   plugins: [react(),tailwindcss()],
 })
 
